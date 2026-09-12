167c5eac75faf1c9e7f6f68541fd77906cacb9e5
Lakshmi Vardhan <honeydogga2013@gmail.com>
2026-09-12 10:08:05 +0530

    Add Week 4 constructors and keywords assignments

---
+++ Assignments/Week4/A4_LibraryMembershipCard.java
@@ -0,0 +1,43 @@
+class MembershipCard {
+
+    static String libraryName;
+    static String validUntil;
+
+    String studentName;
+
+    static {
+        libraryName = "SRM Central Library";
+        validUntil = "May 2027";
+
+        System.out.println("Library info loaded");
+    }
+
+    MembershipCard(String studentName) {
+        this.studentName = studentName;
+    }
+}
+
+public class A4_LibraryMembershipCard {
+
+    public static void main(String[] args) {
+
+        String[] names = {
+            "Ananya",
+            "Rohan",
+            "Priya",
+            "Arjun",
+            "Sneha"
+        };
+
+        for (int i = 0; i < names.length; i++) {
+
+            MembershipCard card =
+                new MembershipCard(names[i]);
+
+            System.out.println(
+                "Membership card issued: "
+                + card.studentName
+            );
+        }
+    }
+}
